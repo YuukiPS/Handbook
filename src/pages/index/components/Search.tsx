85ddd4c48e822fce0764c454280256e79a5eeb90
ElaXan <zero@elaxan.com>
2024-08-18 07:36:56 +0700

    fix: resolve an error "Invalid hook call"

--- src/pages/index/components/Search.tsx
+++ src/pages/index/components/Search.tsx
@@ -162,16 +162,16 @@ const Search: React.FC<SearchProps> = ({ loadGI, loadSR, currentLanguage, state,
 			}
 		}
 
-		const options: OpenDialogOptions = useMemo(() => {
+		const options = (): OpenDialogOptions => {
 			const currentPlatform = platform()
 			return {
 				directory: false,
 				title: 'Select GM Handbook path',
-				filters: currentPlatform === 'windows' ? [{ name: 'GM Handbook', extensions: ['json'] }] : undefined,
+				filters: currentPlatform === 'windows' ? [{name: 'GM Handbook', extensions: ['json']}] : undefined,
 			}
-		}, [])
+		}
 
-		const path = await open(options)
+		const path = await open(options())
 		if (!path) {
 			toast({
 				title: 'No path selected',
