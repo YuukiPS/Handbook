6cea5740eb2acf0e7c41391a1d13aeffaf689dd6
ElaXan <elaxan@protonmail.com>
2024-10-08 13:59:51 +0700

    feat(i18n): add error page translations

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -3,24 +3,24 @@
 import { Button } from '@/components/ui/button'
 import { RefreshCcw } from 'lucide-react'
 import { memo } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
 
 interface ErrorProps {
 	error: Error | null
 }
 
-const ErrorHandling: React.FC<ErrorProps> = memo(
-	({ error }): JSX.Element => (
+const ErrorHandling: React.FC<ErrorProps> = memo(({ error }): JSX.Element => {
+	const { t } = useTranslation('default', { keyPrefix: 'error' })
+	return (
 		<div className='min-h-screen flex justify-center px-4 py-8 bg-background text-foreground'>
 			<div className='max-w-xl w-full space-y-8 text-center'>
 				<div className='space-y-4'>
-					<h1 className='text-4xl font-bold'>Oops! Something went wrong</h1>
-					<p className='text-lg text-muted-foreground'>Sorry about that. We've hit a bump in the road.</p>
+					<h1 className='text-4xl font-bold'>{t('title')}</h1>
+					<p className='text-lg text-muted-foreground'>{t('description')}</p>
 				</div>
 				<div className='bg-card text-card-foreground rounded-lg shadow-lg p-6'>
-					<h2 className='text-xl font-semibold mb-2'>Error Details</h2>
-					<p className='text-sm text-muted-foreground break-words'>
-						{error?.message || 'Unknown error occurred'}
-					</p>
+					<h2 className='text-xl font-semibold mb-2'>{t('details')}</h2>
+					<p className='text-sm text-muted-foreground break-words'>{error?.message || t('unknown')}</p>
 				</div>
 				<div className='space-y-4'>
 					<Button
@@ -30,19 +30,21 @@ const ErrorHandling: React.FC<ErrorProps> = memo(
 						className='inline-flex items-center justify-center'
 					>
 						<RefreshCcw className='mr-2 h-4 w-4' />
-						Try again
+						{t('button')}
 					</Button>
 					<p className='text-sm text-muted-foreground'>
-						If you keep seeing this error, please let us know on{' '}
-						<a className='underline' href='https://github.com/YuukiPS/Handbook/issues'>
-							GitHub
-						</a>
-						. We appreciate your help in improving our app.
+						<Trans
+							i18nKey={'report'}
+							t={t}
+							// biome-ignore lint/a11y/useAnchorContent: The anchor content is provided by the Trans component
+							// biome-ignore lint/correctness/useJsxKeyInIterable: Keys are not needed for static content in Trans component
+							components={[<a className='underline' href='https://github.com/YuukiPS/Handbook/issues' />]}
+						/>
 					</p>
 				</div>
 			</div>
 		</div>
 	)
-)
+})
 
 export default ErrorHandling
